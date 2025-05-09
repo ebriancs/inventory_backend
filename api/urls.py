4c06bf116025c27f7461645637ec6c52fb06b59d
ebriancs <ebrian.cs@gmail.com>
2025-05-09 20:43:51 +0800

    auth setup using jwt

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from .views import (
     ProductListCreateAPIView,
     ProductDetailAPIView,
-    login,
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,    # View to get the access and refresh tokens
+    TokenRefreshView,       # View to refresh the access token
 )
 
 urlpatterns = [
-    path('login/', login),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
+
     path('products/', ProductListCreateAPIView.as_view()),
     path('products/<int:pk>/', ProductDetailAPIView.as_view()),
 ]
