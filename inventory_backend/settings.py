6050aa1d6da9ccaf35320a086770b4cf6b1d0fb8
ebriancs <ebrian.cs@gmail.com>
2025-05-10 17:00:23 +0800

    minimal changes

--- inventory_backend/settings.py
+++ inventory_backend/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
