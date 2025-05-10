135de967493d8557818f884c3f7cf4a7038b3926
ebriancs <ebrian.cs@gmail.com>
2025-05-10 11:13:38 +0800

    minimal changes.

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,7 @@ from .models import Product
 
 @admin.register(Product)  # This is a simpler way to register the model
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'quantity', 'price')
+    list_display = ('id', 'name', 'description', 'quantity', 'price')
     search_fields = ('name',)
     list_filter = ('quantity', 'price')
 
